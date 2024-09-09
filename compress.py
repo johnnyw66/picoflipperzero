5d9d49510fcaf6a615a30ba09c18802b822516dd
John <johnnyw66@gmail.com>
2024-09-09 21:46:33 +0100

    Update compress.py

--- compress.py
+++ compress.py
@@ -1,6 +1,9 @@
 import struct
 import time
 import machine
+import rp2
+from machine import Pin
+  
 import gc
 
 gc.collect()
@@ -8,6 +11,27 @@ gc.collect()
 free_ram = gc.mem_free()
 
 print("Free RAM:bytes", free_ram)
+
+# PIO to play out data entry - 
+@rp2.asm_pio(in_shiftdir=rp2.PIO.SHIFT_RIGHT, out_shiftdir=rp2.PIO.SHIFT_LEFT, set_init=rp2.PIO.OUT_LOW)
+def pulse():
+    # Pull a 32-bit value into the OSR (Output Shift Register)
+    pull(block)
+
+    in_(osr, 16)  # Shift right by 16 bits, ISR now contains bottom 16 bits of OSR in its top 16 bits
+    in_(isr, 15)  # Shift right ISR by 15 bits into ISR --> (OSR & 0xFFFF)=<<1
+    mov(y, isr)    #   y = BOTTOM 16 bits * 2
+    out(x,17)  #   x = TOP 16 bits * 2
+
+
+    set(pins, 1)  # 1 cycle: Set the GPIO pin high
+    label("high_loop")
+    jmp(x_dec, "high_loop")  # 2 cycles per iteration: 1 for decrement, 1 for jump
+
+    set(pins, 0)  # 1 cycle: Set the GPIO pin high
+    label("low_loop")
+    jmp(y_dec, "low_loop")  # 2 cycles per iteration: 1 for decrement, 1 for jump
+
 
 def str_arr_to_bytearray(data):
     compressed_data = bytearray()
@@ -54,6 +78,20 @@ def usleep(v):
 
 def xmit_compressed_flat(data,gpio_pin = 16):
     pin = machine.Pin(gpio_pin, machine.Pin.OUT)
+    # Create a StateMachine instance and load the PIO program
+    sm = rp2.StateMachine(0, pulse, freq=2000000, set_base=Pin(gpio_pin))
+    # Start the StateMachine
+    sm.active(1)
+    for i in range(0, len(data), 4):
+        chunk = data[i:i+4]
+        value = struct.unpack('>I', chunk)[0]
+        sm.put(value)
+        
+    sm.active(0)
+    pin.value(0)
+    
+def xmit_compressed_flat_deprecated(data,gpio_pin = 16):
+    pin = machine.Pin(gpio_pin, machine.Pin.OUT)
 
     for i in range(0, len(data), 4):
         chunk = data[i:i+4]
@@ -80,5 +118,4 @@ pd = process_file(file_path)
 while True:
     xmit_compressed_data(pd, 2, gpio_pin=16)
     time.sleep(4)
-
 
